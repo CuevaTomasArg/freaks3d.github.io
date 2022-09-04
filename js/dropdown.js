1349c5c09f46c1c83b12193d3ed42528e1f78b28
Cueva Tomas Santiago <cuevatomass02@gmail.com>
2022-09-04 08:03:43 -0300

    Comienzo del diseño para destkop

---
+++ js/dropdown.js
@@ -0,0 +1,17 @@
+const dropDowns = document.querySelectorAll(".text-select");
+dropDowns.forEach((dropDown, index) => {
+    dropDown.addEventListener("click", handleOnClickDropDown);
+});
+function handleOnClickDropDown(e) {
+    e.target.parentNode.children[1].classList.toggle("hide");
+}
+
+const menus = document.querySelectorAll(".menu-select li");
+menus.forEach((menu, index) => {
+    menu.addEventListener("click", handleOnClickMenu);
+});
+function handleOnClickMenu(e) {
+    e.target.parentNode.parentNode.classList.toggle("hide");
+    var text = e.target.innerText;
+    e.target.parentNode.parentNode.parentNode.children[0].children[0].innerText = text;
+}
